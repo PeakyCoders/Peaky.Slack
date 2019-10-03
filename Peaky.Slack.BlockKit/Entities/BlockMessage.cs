1708590c8ce662646be85f30e248656bcc126011
SnakyBeaky <urimr26cat@gmail.com>
2019-10-03 17:44:30 +0200

    Remove unused TextType and add block message wrapper

---
+++ Peaky.Slack.BlockKit/Entities/BlockMessage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Peaky.Slack.BlockKit.Abstractions;
+
+namespace Peaky.Slack.BlockKit.Entities
+{
+    public class BlockMessage
+    {
+        [JsonProperty("blocks")]
+        public List<ILayoutBlock> Blocks;
+    }
+}
